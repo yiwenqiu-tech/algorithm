58eec51c635826a4fa148b55c625f648ff90dcda
yiwenqiu <yiwenqiu@tencent.com>
2022-08-01 14:02:04 +0800

    异或

--- xor/find_odd_number.go
+++ xor/find_odd_number.go
@@ -10,23 +10,21 @@ func FindOnlyOneOddNumber(array []int) int {
 }
 
 // 给定N个数，其中只有两个数出现了奇数次，找出这两个奇数
-func FindOnlyTwoOddNumber(array []int) int {
-	//xorResult := 0
-	//for index := range array {
-	//	xorResult ^= array[index]
-	//}
-	//firstNoZeroIndex := 0
-	//for {
-	//	if (xorResult >> firstNoZeroIndex) & 1 == 0 {
-	//		firstNoZeroIndex++
-	//	}else {
-	//		break
-	//	}
-	//}
-	//for index := range array {
-	//	if array[index] >> firstNoZeroIndex {
-	//
-	//	}
-	//}
-	//return oddNum
+func FindOnlyTwoOddNumber(array []int) (int, int) {
+	xorResult := 0
+	for index := range array {
+		xorResult ^= array[index]
+	}
+	// 找出异或后结果，从右边往左算的第一个为1的数字。
+	resultForFirstNoZeroIndex := xorResult & (^xorResult + 1)
+	a := 0
+	for index := range array {
+		// 根据异或后结果，从右边往左算的第一个为1的数字对原数组进行分组
+		if array[index]&resultForFirstNoZeroIndex == 1 {
+			a ^= array[index]
+		}
+	}
+	// 找出a之后，a^b^a = (a^a)^b = 0^b = b
+	b := xorResult ^ a
+	return a, b
 }
--- xor/find_odd_number_test.go
+++ xor/find_odd_number_test.go
@@ -27,3 +27,35 @@ func TestFindOnlyOneOddNumber(t *testing.T) {
 		})
 	}
 }
+
+func TestFindOnlyTwoOddNumber(t *testing.T) {
+	type args struct {
+		array []int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 int
+	}{
+		{
+			name: "find only two odd number",
+			args: args{
+				array: []int{1, 1, 2, 2, 3, 4},
+			},
+			want:  3,
+			want1: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := FindOnlyTwoOddNumber(tt.args.array)
+			if got != tt.want {
+				t.Errorf("FindOnlyTwoOddNumber() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("FindOnlyTwoOddNumber() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
